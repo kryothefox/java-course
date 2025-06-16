b940fd9ee3cfe80faede09e0cc06438ac3e9cbf1
kryothefox <39372576+kryothefox@users.noreply.github.com>
2025-06-16 11:42:56 +0200

    create own exceptions

---
+++ dia 5/com/core/Errors/zeroOnSecondValue.java
@@ -0,0 +1,10 @@
+package com.core.Errors;
+
+public class zeroOnSecondValue extends NumberFormatException {
+
+    public zeroOnSecondValue(){
+        super("Cannot have 0 as a second number");
+        
+    }
+
+    }
--- dia 5/com/core/init.java
+++ dia 5/com/core/init.java
@@ -1,21 +1,27 @@
 package com.core;
 
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import com.core.Errors.zeroOnSecondValue;
+
+@SuppressWarnings("unused")
 public class init {
     public static void main(String[] args) {
 
         // **** ITERABLES AND COLLECTIONS ****
 
-        List<Integer> intList = Arrays.asList(new Integer[] { 4, 2, 1, 5, 2 });
-
-        Iterator<Integer> iter = intList.iterator();
-
         /*
+         * List<Integer> intList = Arrays.asList(new Integer[] { 4, 2, 1, 5, 2 });
+         * 
+         * Iterator<Integer> iter = intList.iterator();
+         * 
+         * 
          * while (iter.hasNext()) {
          * Integer value = iter.next();
          * System.out.println(value);
@@ -24,57 +30,62 @@ public class init {
 
         // **** EXCEPTIONS AND CATCHING
 
-        try {
+        /*
+         * try {
+         * 
+         * Scanner sc = new Scanner(System.in);
+         * System.out.println("input 2 numbers");
+         * 
+         * double val = sc.nextDouble();
+         * double val2 = sc.nextDouble();
+         * sc.close(); // Java GC, I SUMMON YOU!!!
+         * 
+         * if (val2 == 0.0) {
+         * throw new ArithmeticException("diving by 0 you dingus");
+         * }
+         * ;
+         * 
+         * System.out.println(val / val2);
+         * 
+         * } catch (InputMismatchException | IllegalStateException ex) {
+         * 
+         * System.out.println("bad input provided!\n" + ex);
+         * ex.printStackTrace();
+         * System.exit(1);
+         * 
+         * } catch (ArithmeticException ex) {
+         * 
+         * System.out.println("bad calculation!\n" + ex);
+         * ex.printStackTrace();
+         * 
+         * } finally {
+         * 
+         * System.out.println("prg finished"); // executes no matter what, unless
+         * unhandled exception
+         * 
+         * }
+         */
 
-            Scanner sc = new Scanner(System.in);
-            System.out.println("input 2 numbers");
-
-            double val = sc.nextDouble();
-            double val2 = sc.nextDouble();
-            sc.close(); // Java GC, I SUMMON YOU!!!
-
-            if (val2 == 0.0) {
-                throw new ArithmeticException("diving by 0 you dingus");
-            }
-            ;
-
-            System.out.println(val / val2);
-
-        } catch (InputMismatchException | IllegalStateException ex) {
-
-            System.out.println("bad input provided!\n" + ex);
-            ex.printStackTrace();
-
-        } catch (ArithmeticException ex) {
-
-            System.out.println("bad calculation!\n" + ex);
-            ex.printStackTrace();
-
-        } finally {
-
-            System.out.println("prg finished"); // executes no matter what, unless unhandled exception
-
-        }
-
-        Scanner sc = new Scanner(System.in);
-        subtract(sc);
+        
+        Calculator meow = new Calculator();
+        System.out.println(meow.subtract(250,0));
 
     }
 
-    public static void subtract(Scanner scanner) {
+    public static class Calculator {
+        public double subtract(double a, double b)
+                throws ArithmeticException,
+                NumberFormatException,
+                NullPointerException,
+                NoSuchElementException,
+                zeroOnSecondValue {
 
-        try {
             System.out.println("Input 2 numbers");
-            Double val1 = scanner.nextDouble();
-            Double val2 = scanner.nextDouble();
-            scanner.close();
-            System.out.println(val1 - val2);
+            if(b == 0.0){throw new zeroOnSecondValue();};
+            return (a - b);
 
-        } catch (Exception e) {
-            System.out.println(e);
-            e.printStackTrace();
         }
-
+        
     }
 
 }
