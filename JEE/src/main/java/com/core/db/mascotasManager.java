34618ba0aca7eea208353309d6443188885b6124
kryothefox <39372576+kryothefox@users.noreply.github.com>
2025-06-25 11:59:01 +0200

    add method

--- JEE/src/main/java/com/core/db/mascotasManager.java
+++ JEE/src/main/java/com/core/db/mascotasManager.java
@@ -1,5 +1,8 @@
 package com.core.db;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -20,5 +23,18 @@ public class mascotasManager {
         return (Mascotas) query1.setParameter("idMascota",ID).getSingleResult();
 
     }
+
+    @SuppressWarnings("unchecked")
+    public List<Mascotas> findByLimitAndRace(int limit, String filterRace){
+        EntityManager em = emf.createEntityManager();
+        Query q1 = em.createNamedQuery("Mascotas.findAll");
+
+        Stream<Mascotas> stream = q1.getResultStream();
+        return stream.filter((Mascotas m) -> m.getActivo())
+        .limit(limit)
+        .toList();
+
+    }
+
 
 }
